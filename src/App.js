b4d5bc02146210381620324dd756a6ffcba3a555
Hyper-ActiveX <jayactive11112@gmail.com>
2022-07-10 19:57:26 +0530

    Created app and added images and navbar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar/Navbar";
+import Intro from "./components/Intro/Intro";
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Intro/>
+    </div>
+  );
+}
+
+export default App;
