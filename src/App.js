ca110ccdb15cc621b785ddf50c8a877d0d621acc
Hyper-ActiveX <jayactive11112@gmail.com>
2022-07-11 21:32:15 +0530

    Completed Services pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Navbar from "./components/Navbar/Navbar";
 import Intro from "./components/Intro/Intro";
 
 import './App.css'
+import Services from "./components/Services/Services";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Intro/>
+      <Services/>
     </div>
   );
 }
--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -6,7 +6,7 @@ import boy from "../../img/boy.png";
 import glassesimoji from "../../img/glassesimoji.png";
 import thumbup from "../../img/thumbup.png";
 import crown from "../../img/crown.png";
-import FloatingDiv from "../FloatingDiv/FloatingDiv";
+// import FloatinDiv from "../FloatingDiv/FloatingDiv";
 import Github from "../../img/github.png";
 import LinkedIn from "../../img/linkedin.png";
 import Instagram from "../../img/instagram.png";
@@ -14,12 +14,12 @@ import { themeContext } from "../../Context";
 import { motion } from "framer-motion";
 import { Link } from "react-scroll";
 const Intro = () => {
-  
-//   const transition = { duration: 2, type: "spring" };
+  // Transition
+  const transition = { duration: 2, type: "spring" };
 
-  
-//   const theme = useContext(themeContext);
-//   const darkMode = theme.state.darkMode;
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
 
   return (
     <div className="Intro" id="Intro">
@@ -27,16 +27,16 @@ const Intro = () => {
       <div className="i-left">
         <div className="i-name">
           {/* yahan change hy darkmode ka */}
-          <span >Hy! I Am</span>
-          <span>JAY</span>
+          <span style={{ color: darkMode ? "white" : "" }}>Hy! I Am</span>
+          <span>Andrew Thomas</span>
           <span>
             Frontend Developer with high level of experience in web designing
             and development, producting the Quality work
           </span>
         </div>
-        {/* <Link to="contact" smooth={true} spy={true}> */}
+        <Link to="contact" smooth={true} spy={true}>
           <button className="button i-button">Hire me</button>
-        {/* </Link> */}
+        </Link>
         {/* social icons */}
         <div className="i-icons">
           <img src={Github} alt="" />
@@ -46,39 +46,38 @@ const Intro = () => {
       </div>
       {/* right image side */}
       <div className="i-right">
-        <img src={Vector1} alt="" />
+        {/* <img src={Vector1} alt="" />
         <img src={Vector2} alt="" />
         <img src={boy} alt="" />
-        {/* animation */}
-        {/* <motion.img
+        
+        <motion.img
           initial={{ left: "-36%" }}
           whileInView={{ left: "-24%" }}
           transition={transition}
           src={glassesimoji}
           alt=""
-        /> */}
+        />
 
-        {/* <motion.div
+        <motion.div
           initial={{ top: "-4%", left: "74%" }}
           whileInView={{ left: "68%" }}
           transition={transition}
           className="floating-div"
-        > */}
-          <FloatingDiv img={crown} text1="Web" text2="Developer" />
-        {/* </motion.div> */}
+        >
+          <FloatinDiv img={crown} text1="Web" text2="Developer" />
+        </motion.div>
 
-        {/* animation */}
-        {/* <motion.div
+        
+        <motion.div
           initial={{ left: "9rem", top: "18rem" }}
           whileInView={{ left: "0rem" }}
           transition={transition}
           className="floating-div"
-        > */}
-          {/* floatinDiv mein change hy dark mode ka */}
-          <FloatingDiv img={thumbup} text1="Best Design" text2="Award" />
-        {/* </motion.div> */}
+        >
+          
+          <FloatinDiv img={thumbup} text1="Best Design" text2="Award" />
+        </motion.div>
 
-    {/* bluring */}
         <div className="blur" style={{ background: "rgb(238 210 255)" }}></div>
         <div
           className="blur"
@@ -89,8 +88,7 @@ const Intro = () => {
             height: "11rem",
             left: "-9rem",
           }}
-        ></div>
-        
+        ></div> */}
       </div>
     </div>
   );
